f7925f372d2a780db9e0079b780318a7a09aa466
pat <pat@pat-new-VirtualBox.(none)>
2012-10-31 23:37:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿function doChild(me,childID){
+  //alert("- "+me+" -");
+  if (me) {
+    hide(childID);
+  } else {
+    show(childID);
+  }
+}
+//////////////////////////////////////////////////////////////
+function hide(childID) {
+	document.getElementById(childID).style.display = 'none';
+}
+//////////////////////////////////////////////////////////////
+function show(childID) {
+	document.getElementById(childID).style.display = 'block';
+}
+//////////////////////////////////////////////////////////////
+function testChars() {
+  var f=document.getElementById("content");
+  var value = f.value.length;
+  if (value<6) {
+    alert("The message must have at least 6 chars.");
+    return false;
+  }
+  if (value>160) {
+    alert("The message cannot have more than 160 chars.");
+    return false;
+  }
+    return true;
+}
+//////////////////////////////////////////////////////////////
+function showChars(f) {
+    f.charsCount.value = f.content.value.length;
+    return true;
+}
